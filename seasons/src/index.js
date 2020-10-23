895e2549e632da47513de0dc1dba768e44ff0d4a
Danielle Byrne <DanielleByrne421@gmail.com>
2020-10-23 16:11:27 -0400

    adding conditional rendering to show loading, error, or latitude

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -16,13 +16,17 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        Latitude:{this.state.lat}
-        <br />
-        Error:{this.state.errorMessage}
-      </div>
-    );
+    //if we have an error message and no lat
+    if(this.state.errorMessage && !this.state.lat){
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+    //if we don't have an error message but we do have the lat
+    if(!this.state.errorMessage && this.state.lat) {
+    return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading!</div>
+
   }
 }
 
