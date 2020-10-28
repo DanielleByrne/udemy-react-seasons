8cc6f707a52dc6eb9a539960a70b9d98115d12f6
Danielle Byrne <DanielleByrne421@gmail.com>
2020-10-28 16:54:38 -0400

    starting to add the season dependent styling to the page

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,17 +1,22 @@
 import React from "react";
 
 const getSeason = (lat, month) => {
-if (month > 2 && month < 9){
-  return lat > 0 ? 'summer' : 'winter';
-}else {
-  return lat > 0 ? 'winter' : 'summer';
-}
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
 };
 
 const SeasonDisplay = (props) => {
-  const season = getSeason(props.lat, new Date().getMonth())
-  console.log(season)
-  return <div>Season Display</div>;
+  const season = getSeason(props.lat, new Date().getMonth());
+  // console.log(season)
+
+  return (
+    <div>
+      {season === "winter" ? "Brr it's chilly" : "Lets go to the beach"}
+    </div>
+  );
 };
 
 export default SeasonDisplay;
